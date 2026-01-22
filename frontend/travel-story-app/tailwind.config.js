529697372e7d75c64e401fa9d660e561315d3a24
Abdullah <abdullahaamir197@gmail.com>
2026-01-22 12:30:40 +0500

    Add project files

---
+++ frontend/travel-story-app/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    fontFamily: {
+      display:"poppins", "sans-serif",
+
+    },
+    extend: {
+      //colors used in the project
+      colors:{
+        primary:"#058603", 
+        secondary:"#EF863E",
+      },
+    },
+  },
+  plugins: [],
+}
+
